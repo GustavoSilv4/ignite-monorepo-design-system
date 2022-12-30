4c436e393b648b98ecb556a09492e67b53eecbb7
Gustavo Santos <gustavinhodossantos123@gmail.com>
2022-12-30 19:44:52 -0300

    Initial commit

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from 'react'
+
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  padding: '$2 $4',
+  border: 'none',
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: '$sm',
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: '$lg',
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
